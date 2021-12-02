1308f64bacd5664a0abaf91aea54bbc1c176ef51
Adam Wonia <adam.wonia@gmail.com>
2021-12-02 17:46:32 +0100

    ReadNumber() function created

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,10 +10,33 @@ namespace Project
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(ReadNumber());
 
 
             Console.ReadKey();
         }
+        /// <summary>
+        /// Get number from user between -1000 and 1000
+        /// </summary>
+        /// <returns>Return given number</returns>
+        public static int ReadNumber()
+        {
+            int inputNumber = 0;
+            bool continueFlag = true;
+
+            Console.WriteLine("\nInsert an integer between -1000 and 1000:");
+            while (continueFlag)
+            {
+                inputNumber = int.Parse(Console.ReadLine());
+                if (inputNumber > 1000 || inputNumber < -1000)
+                    Console.WriteLine("Wrong number! Try again :)");
+                else
+                    continueFlag = false;
+            }
+            return inputNumber;
+        }
+
+
+
     }
 }
